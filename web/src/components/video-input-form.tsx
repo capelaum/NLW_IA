cd93a5540bd9fe33ae75ac5f4030bfbc11d20f5e
capelaum <thecapellett@gmail.com>
2023-09-14 21:01:03 -0300

    feat: generate video preview URL

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -1,22 +1,61 @@
 import { FileVideo, Upload } from 'lucide-react'
+import { ChangeEvent, useMemo, useState } from 'react'
 import { Button } from './ui/button'
 import { Label } from './ui/label'
 import { Separator } from './ui/separator'
 import { Textarea } from './ui/textarea'
 
 export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+
+  function handleFileSelected(e: ChangeEvent<HTMLInputElement>) {
+    const { files } = e.currentTarget
+
+    if (!files) {
+      return
+    }
+
+    const selectedFile = files[0]
+
+    setVideoFile(selectedFile)
+  }
+
+  const previewUrl = useMemo(() => {
+    if (!videoFile) {
+      return null
+    }
+
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
   return (
     <form className="space-y-5">
       <label
         htmlFor="video"
         title="Carregar vídeo"
-        className="flex aspect-video w-full flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground transition-all duration-200 hover:cursor-pointer hover:border-rose-400 hover:bg-primary/5 hover:text-rose-400"
+        className="relative flex aspect-video w-full flex-col items-center justify-center gap-2 rounded-md border border-dashed text-sm text-muted-foreground transition-all duration-200 hover:cursor-pointer hover:border-rose-400 hover:bg-primary/5 hover:text-rose-400"
       >
-        <FileVideo className="h-4 w-4" />
-        Seleciona um vídeo
+        {previewUrl ? (
+          <video
+            src={previewUrl}
+            controls={false}
+            className="pointer-events-none absolute inset-0 h-full w-full"
+          />
+        ) : (
+          <>
+            <FileVideo className="h-4 w-4" />
+            Seleciona um vídeo
+          </>
+        )}
       </label>
 
-      <input type="file" id="video" accept="video/mp4" className="sr-only" />
+      <input
+        type="file"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
 
       <Separator />
 
